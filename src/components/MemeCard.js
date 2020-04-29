bd8f4a5d420a18e10c88082a5bb637b6d7ae6dbf
amandena <manda.panda91@yahoo.com>
2020-04-29 00:18:55 -0400

    added classes to svg elements

--- src/components/MemeCard.js
+++ src/components/MemeCard.js
@@ -2,20 +2,20 @@ import React from 'react'
 
 const MemeCard = () => {
   return(
-    <div contentEditable='true'>
+    <div contentEditable='true' className="meme-div">
       <svg version="1.1"
           baseProfile="full"
           width="500" height="400"
           // viewBox="0 0 100 100"
           xmlns="http://www.w3.org/2000/svg">
 
-        <rect width="100%" height="100%" fill="black" />
+        <rect class="background-box" width="100%" height="100%" fill="black" />
 
-        <text x="250" y="43" font-size="55" text-anchor="middle" fill="white">YOUR TEXT HERE</text>
+        <text class="top-text" x="250" y="43" font-size="55" text-anchor="middle" fill="white">YOUR TEXT HERE</text>
 
-        <rect width="100%" height="75%" x="" y="50" fill="white" />
+        <rect class="img-box" width="100%" height="75%" x="" y="50" fill="white" stroke="black" stroke-width="2" />
 
-        <text x="250" y="393" font-size="55" text-anchor="middle" fill="white">YOUR TEXT HERE</text>
+        <text class="bottom-text" x="250" y="393" font-size="55" text-anchor="middle" fill="white">YOUR TEXT HERE</text>
 
       </svg>
     </div>
