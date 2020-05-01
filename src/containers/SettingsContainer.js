f80c0fb68b3e10e7ae7c7c78d321f5854359cb51
amandena <manda.panda91@yahoo.com>
2020-04-30 23:19:01 -0400

    added buttons and labels

--- src/containers/SettingsContainer.js
+++ src/containers/SettingsContainer.js
@@ -3,8 +3,31 @@ import React from 'react'
 class SettingsContainer extends React.Component {
   render() {
     return(
-      <div>
-        SettingsContainer
+      <div className='settings'>
+        <label>OUTPUT SIZE</label><br/>
+          <button>1:1</button>
+          <button>3:4</button>
+          <button>4:3</button>
+          <button>3:5</button>
+          <button>5:3</button>
+
+        <br/><br/>
+
+        <label>BACKGROUND COLOR</label><br/>
+          <button>BLACK</button>
+          <button>BLUE</button>
+          <button>GREEN</button>
+          <button>YELLOW</button>
+          <button>WHITE</button>
+
+        <br/><br/>
+
+        <label>FONT</label><br/>
+          <button>BOLD</button>
+          <button>CURSIVE</button>
+          <button>SERIF</button>
+          <button>SANS-SERIF</button>
+          <button>WHIMSY</button>
       </div>
     )
   }
