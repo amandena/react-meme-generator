e78ab864b73fbc98d7a678701843888ab5ae1ff1
amandena <manda.panda91@yahoo.com>
2020-05-02 19:18:13 -0400

    added class names and a save changes button

--- src/containers/SettingsContainer.js
+++ src/containers/SettingsContainer.js
@@ -1,33 +1,45 @@
 import React from 'react'
 
 class SettingsContainer extends React.Component {
+  
+  
   render() {
     return(
       <div className='settings'>
-        <label>OUTPUT SIZE</label><br/>
-          <button>1:1</button>
-          <button>3:4</button>
-          <button>4:3</button>
-          <button>3:5</button>
-          <button>5:3</button>
+        <div className='sizes'>
+          <label>OUTPUT SIZE</label><br/>
+            <button className='one-by-one'>1:1</button>
+            <button className='three-by-four'>3:4</button>
+            <button className='four-by-three'>4:3</button>
+            <button className='three-by-five'>3:5</button>
+            <button className='five-by-three'>5:3</button>
+        </div>
 
         <br/><br/>
 
-        <label>BACKGROUND COLOR</label><br/>
-          <button>BLACK</button>
-          <button>BLUE</button>
-          <button>GREEN</button>
-          <button>YELLOW</button>
-          <button>WHITE</button>
+        <div className='colors'>
+          <label>BACKGROUND COLOR</label><br/>
+            <button className='black'>BLACK</button>
+            <button className='blue'>BLUE</button>
+            <button className='green'>GREEN</button>
+            <button className='yellow'>YELLOW</button>
+            <button className='white'>WHITE</button>
+        </div>
 
         <br/><br/>
 
-        <label>FONT</label><br/>
-          <button>BOLD</button>
-          <button>CURSIVE</button>
-          <button>SERIF</button>
-          <button>SANS-SERIF</button>
-          <button>WHIMSY</button>
+        <div className='fonts'>
+          <label>FONT</label><br/>
+            <button className='bold'>BOLD</button>
+            <button className='cursive'>CURSIVE</button>
+            <button className='serif'>SERIF</button>
+            <button className='sans-serif'>SANS-SERIF</button>
+            <button className='whimsy'>WHIMSY</button>
+        </div>
+
+        <br/><br/>
+
+        <button className='save'>Save Changes</button>
       </div>
     )
   }
