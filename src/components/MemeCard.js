9b76db918bee93fd4555b001c9464113a5232052
amandena <manda.panda91@yahoo.com>
2020-05-05 23:48:52 -0400

    removed comment

--- src/components/MemeCard.js
+++ src/components/MemeCard.js
@@ -6,7 +6,6 @@ const MemeCard = () => {
       <svg version="1.1"
           baseProfile="full"
           width="500" height="400"
-          // viewBox="0 0 100 100"
           xmlns="http://www.w3.org/2000/svg">
 
         <rect className="background-box" width="100%" height="100%" fill="black" />
