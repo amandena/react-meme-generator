4aeea0c5140b797abcf12d4daa598b3b710f194c
amandena <manda.panda91@yahoo.com>
2020-05-05 23:51:10 -0400

    added constructor and passed props into settings container component

--- src/containers/MemeContainer.js
+++ src/containers/MemeContainer.js
@@ -4,13 +4,22 @@ import MemeCard from '../components/MemeCard'
 import UploadForm from '../components/UploadForm'
 
 class MemeContainer extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      size: '3:4',
+      color: 'black',
+      font: 'bold'
+    }
+  }
+  
   render() {
     return(
       <div className='meme-container'>
-        MemeContainer
+        <h1>Make a Meme</h1>
         <MemeCard/>
         <UploadForm/>
-        <SettingsContainer/>
+        <SettingsContainer size={this.state.size} color={this.state.color} font={this.state.font}/>
       </div>
     )
   }
