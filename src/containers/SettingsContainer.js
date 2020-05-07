4a6081d0b4c9d90ddaf3162f776b2f998dfc5527
amandena <manda.panda91@yahoo.com>
2020-05-06 22:33:13 -0400

    converted to a functional component

--- src/containers/SettingsContainer.js
+++ src/containers/SettingsContainer.js
@@ -1,49 +1,45 @@
 import React from 'react'
 
-class SettingsContainer extends React.Component {
-  
-  render() {
-    const { size } = this.props
+const SettingsContainer = ({ handleSizeChange }) => {
 
-    return(
-      <div className='settings'>
-        <div className='sizes'>
-          <label>OUTPUT SIZE</label><br/>
-            <button className='one-by-one' name='1:1' value={size} onClick={this.handleSizeChange}>1:1</button>
-            <button className='three-by-four' name='3:4' value={size} onClick={this.handleSizeChange}>3:4</button>
-            <button className='four-by-three' name='4:3' value={size} onClick={this.handleSizeChange}>4:3</button>
-            <button className='three-by-five' name='3:5' value={size} onClick={this.handleSizeChange}>3:5</button>
-            <button className='five-by-three' name='5:3' value={size} onClick={this.handleSizeChange}>5:3</button>
-        </div>
-
-        <br/><br/>
-
-        <div className='colors'>
-          <label>BACKGROUND COLOR</label><br/>
-            <button className='black'>BLACK</button>
-            <button className='blue'>BLUE</button>
-            <button className='green'>GREEN</button>
-            <button className='yellow'>YELLOW</button>
-            <button className='white'>WHITE</button>
-        </div>
-
-        <br/><br/>
-
-        <div className='fonts'>
-          <label>FONT</label><br/>
-            <button className='bold'>BOLD</button>
-            <button className='cursive'>CURSIVE</button>
-            <button className='serif'>SERIF</button>
-            <button className='sans-serif'>SANS-SERIF</button>
-            <button className='whimsy'>WHIMSY</button>
-        </div>
-
-        <br/><br/>
-
-        <button className='save'>Save Changes</button>
+  return(
+    <div className='settings'>
+      <div className='sizes'>
+        <label>OUTPUT SIZE</label><br/>
+          <button className='one-by-one' name='1:1' onClick={handleSizeChange}>1:1</button>
+          <button className='three-by-four' name='3:4' onClick={handleSizeChange}>3:4</button>
+          <button className='four-by-three' name='4:3' onClick={handleSizeChange}>4:3</button>
+          <button className='three-by-five' name='3:5' onClick={handleSizeChange}>3:5</button>
+          <button className='five-by-three' name='5:3' onClick={handleSizeChange}>5:3</button>
       </div>
-    )
-  }
+
+      <br/><br/>
+
+      <div className='colors'>
+        <label>BACKGROUND COLOR</label><br/>
+          <button className='black'>BLACK</button>
+          <button className='blue'>BLUE</button>
+          <button className='green'>GREEN</button>
+          <button className='yellow'>YELLOW</button>
+          <button className='white'>WHITE</button>
+      </div>
+
+      <br/><br/>
+
+      <div className='fonts'>
+        <label>FONT</label><br/>
+          <button className='bold'>BOLD</button>
+          <button className='cursive'>CURSIVE</button>
+          <button className='serif'>SERIF</button>
+          <button className='sans-serif'>SANS-SERIF</button>
+          <button className='whimsy'>WHIMSY</button>
+      </div>
+
+      <br/><br/>
+
+      <button className='save'>Save Changes</button>
+    </div>
+  )
 }
 
 export default SettingsContainer
