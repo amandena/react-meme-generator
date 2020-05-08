7dcc23184e3718940e150f8fba9ac4072b8bf410
amandena <manda.panda91@yahoo.com>
2020-05-08 17:20:18 -0400

    added constructor with initial state and edited form

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,13 +1,25 @@
 import React from 'react'
 
 class UploadForm extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      uploadForm: {
+        image_url: ''
+      }
+    }
+  }
+
   render() {
     return(
       <div className='upload'>
         <form>
-          <label>Upload an Image: </label>
+          <h3>Upload an Image:</h3>
+
           <input type='text' placeholder='image_url'></input>
-  
+
+          <br/><br/>
+
           <input type='submit' value='Upload'/>
         </form>
       </div>
