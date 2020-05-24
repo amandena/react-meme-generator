1caa9e9a92a9b6cb0ac6f7ad52b7eae3295e1f19
amandena <manda.panda91@yahoo.com>
2020-05-23 23:26:57 -0400

    removed sizing buttons and props

--- src/containers/SettingsContainer.js
+++ src/containers/SettingsContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const SettingsContainer = ({ handleSizeChange, handleChange, topText, bottomText }) => {
+const SettingsContainer = ({ handleChange, topText, bottomText }) => {
 
   return(
     <div className='settings'>
@@ -27,40 +27,6 @@ const SettingsContainer = ({ handleSizeChange, handleChange, topText, bottomText
 
         <br/><br/>
 
-      </div>
-      
-      <div className='sizes'>
-        <label>OUTPUT SIZE</label><br/>
-          <button 
-            className='one-by-one'
-            name='size' 
-            value='1:1' 
-            onClick={handleSizeChange}
-          >1:1</button>
-          <button 
-            className='three-by-four' 
-            name='size'
-            value='3:4' 
-            onClick={handleSizeChange}
-          >3:4</button>
-          <button 
-            className='four-by-three'
-            name='size' 
-            value='4:3' 
-            onClick={handleSizeChange}
-          >4:3</button>
-          <button 
-            className='three-by-five'
-            name='size' 
-            value='3:5' 
-            onClick={handleSizeChange}
-          >3:5</button>
-          <button 
-            className='five-by-three'
-            name='size' 
-            value='5:3' 
-            onClick={handleSizeChange}
-          >5:3</button>
       </div>
 
       <br/>
